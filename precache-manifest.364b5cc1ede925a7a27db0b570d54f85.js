f2b28d23e48e5858084f7dd456d3da163a482f02
Nastya Bobrova <deepkilt89@gmail.com>
2019-08-07 08:59:21 +0300

    Updates

---
+++ precache-manifest.364b5cc1ede925a7a27db0b570d54f85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd3e0ab14479ff089c84b7157213836",
+    "url": "/todo-react-redux/index.html"
+  },
+  {
+    "revision": "7a34ebe140b6192d3f93",
+    "url": "/todo-react-redux/static/css/main.61020777.chunk.css"
+  },
+  {
+    "revision": "2d5e4f9d872ca5547ef2",
+    "url": "/todo-react-redux/static/js/2.8272b1fe.chunk.js"
+  },
+  {
+    "revision": "7a34ebe140b6192d3f93",
+    "url": "/todo-react-redux/static/js/main.0f92d70e.chunk.js"
+  },
+  {
+    "revision": "c2979c65fca7cd9446fe",
+    "url": "/todo-react-redux/static/js/runtime~main.9c2e1894.js"
+  }
+]);
